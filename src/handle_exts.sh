f8d68b246bf5c6e413c37903052c98fef52e058c
Zeno Saletti <zenosaletti@gmail.com>
2024-11-26 17:11:43 +0100

    updates

--- src/handle_exts.sh
+++ src/handle_exts.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+JUNK_DIR=junk
+
+# This script moves all the files with the extensions listed in the array to a junk folder
 extensions=("*.aux" "*.log" "*.sta" "*.fdb_latexmk" "*.fls" "*.synctex.gz" "*.maf" "*.out" "*.toc" "*.mtc*")
 
 for i in ${!extensions[@]}; do
-    mv ${extensions[$i]} junk 2>/dev/null
+    mv ${extensions[$i]} $JUNK_DIR 2>/dev/null
 done
